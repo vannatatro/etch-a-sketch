90afd73c5117e45fef245b565d7fd207d292df34
Savanna <149965311+vannatatro@users.noreply.github.com>
2024-01-24 17:18:33 -0500

    Remove div borders

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 // Make grid of divs
 let divContainer = document.querySelector("#container")
-let amount = 16
 
-function createGrid(amount) {
-} if (amount > 100) {
-    console.log("number too big");
-
-} else if (amount < 2) {
-    console.log("number too small");
-
-} else if (amount <= 100) {
-    for (let i = 1; i < amount+1; i++) {
+const createGrid = (amtOfSquares) => {
+    for (let i = 1; i < amtOfSquares+1; i++) {
         const row = document.createElement("div");
         row.classList.add("gridRow")
         divContainer.append(row);
-        for (let i = 1; i < amount+1; i++) {
+        for (let i = 1; i < amtOfSquares+1; i++) {
             let squares = document.createElement("div");
             squares.classList.add("gridSquares");
             row.append(squares);
@@ -22,8 +14,26 @@ function createGrid(amount) {
             squares.addEventListener('mouseenter',() => {
                 squares.style.backgroundColor = "black"
             })
+        }
     }
-}}
+}
+createGrid(32)
+// Make button that refreshes ink
 
-// Make button that prompts user for grid dimensions
+let resetBtn = document.getElementById("refreshBtn")
+resetBtn.addEventListener("click", () => {
+    console.log("clicked")
+    location.reload();
+})
 
+// Make button for custom dimensions
+// let dimBtn = document.getElementById("sizeBtn")
+// dimBtn.addEventListener('click', () => {
+//     let userAmount = Number(prompt("Please enter the dimensions (2-100) would you like"))
+
+//     while (userAmount > 100) {
+//         userAmount = Number(prompt("Number entered was too big. Please enter the dimensions (2-100) would you like"))
+//     } 
+//     createGrid(userAmount)
+    
+// })
