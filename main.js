bfbc079204c7f8ac67d8dd214647f16bc9c7fccf
Savanna <149965311+vannatatro@users.noreply.github.com>
2024-02-01 15:08:48 -0500

    Add reset button and change css

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Make grid of divs
 let divContainer = document.querySelector("#container")
 
-const createGrid = (amtOfSquares) => {
-    for (let i = 1; i < amtOfSquares+1; i++) {
-        const row = document.createElement("div");
+function createGrid(userAmount) { {
+    for (let i = 1; i < userAmount+1; i++) {
+        let row = document.createElement("div");
         row.classList.add("gridRow")
         divContainer.append(row);
-        for (let i = 1; i < amtOfSquares+1; i++) {
+        for (let i = 1; i < userAmount+1; i++) {
             let squares = document.createElement("div");
             squares.classList.add("gridSquares");
             row.append(squares);
@@ -14,26 +14,46 @@ const createGrid = (amtOfSquares) => {
             squares.addEventListener('mouseenter',() => {
                 squares.style.backgroundColor = "black"
             })
-        }
+            // Make button that refreshes ink
+                let resetBtn = document.getElementById("refreshBtn")
+                resetBtn.addEventListener("click", () => {
+                squares.style.backgroundColor = "white";
+            })
+
+            // Custom size button
+            let dimBtn = document.getElementById("sizeBtn");
+            dimBtn.addEventListener('click', () => { 
+                row.removeChild(squares);
+                divContainer.removeChild(row);
+
+            })
+        }}
+        
+
     }
 }
-createGrid(32)
-// Make button that refreshes ink
 
-let resetBtn = document.getElementById("refreshBtn")
-resetBtn.addEventListener("click", () => {
-    console.log("clicked")
-    location.reload();
-})
+createGrid(36)
 
 // Make button for custom dimensions
-// let dimBtn = document.getElementById("sizeBtn")
-// dimBtn.addEventListener('click', () => {
-//     let userAmount = Number(prompt("Please enter the dimensions (2-100) would you like"))
+let dimBtn = document.getElementById("sizeBtn")
+dimBtn.addEventListener('click', () => {
+    userAmount = Number(prompt("Please enter the dimensions (2-100) would you like"))
 
-//     while (userAmount > 100) {
-//         userAmount = Number(prompt("Number entered was too big. Please enter the dimensions (2-100) would you like"))
-//     } 
-//     createGrid(userAmount)
+    if (userAmount > 100) {
+        alert("Number entered was too big. Please try again.")
+        userAmount = 36;
+    } 
+    if (userAmount < 2) {
+        alert("Number entered was too small. Please try again.")
+        userAmount = 36;
+    }
+
+    if (2 <= userAmount <=100) {
+        createGrid(userAmount)
+    }
+    // row.removeChild(squares)
+    // divContainer.removeChild(row)
+    // createGrid(userAmount)
     
-// })
+})
